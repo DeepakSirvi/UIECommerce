629e63fa302a8c754d55df6a52991669e3e7ab92
DeepakSirvi <deepaksirvi45@gmail.com>
2023-12-06 16:16:22 +0530

    logout

--- src/app/User/Component/navbar/navbar.component.ts
+++ src/app/User/Component/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from 'src/app/Service/AuthService/login.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  
+  constructor(public login:LoginService){}
+    isUserActive=false;
+    user:any=null;
+
+   ngOnInit(){
+    this.isUserActive=this.login.isLoggedIn();
+    this.user=this.login.getUser();
+    this.login.loginStatusSubject.asObservable().subscribe(data=>{
+      this.isUserActive=this.login.isLoggedIn();
+      this.user=this.login.getUser();
+      console.log(this.isUserActive);
+      console.log(this.user);
+    });
+   
+
+   } 
+  public logout(){
+    this.login.logout();
+    window.location.reload();
+  }
   
 }
