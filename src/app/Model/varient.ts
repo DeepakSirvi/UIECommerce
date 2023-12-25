bb9b35d44dcf543458ad32e21e1c97835c353421
DeepakSirvi <deepaksirvi45@gmail.com>
2023-12-25 10:52:23 +0530

    ProductImage

--- src/app/Model/varient.ts
+++ src/app/Model/varient.ts
@@ -1,5 +1,6 @@
 import { ProductRequest } from "../RequestPayload/product-request";
 import { Product } from "./product";
+import { ProductImage } from "./product-image";
 import { VarientCategoryJoin } from "./varient-category-join";
 
 export class Varient {
@@ -10,4 +11,5 @@ export class Varient {
     product: Product=new Product();
     // status: Status;
     categoryJoins: VarientCategoryJoin[]=[];
+    productImage:ProductImage[]=[];
 }
