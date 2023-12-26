81fed57db99409cc2766492f400b5698d774d5ba
DeepakSirvi <deepaksirvi45@gmail.com>
2023-12-26 18:36:01 +0530

    ProductListing

---
+++ src/app/RequestPayload/status-boolean-request.ts
@@ -0,0 +1,5 @@
+export class StatusBooleanRequest {
+    id!:number;
+    status!:boolean;
+}
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -172,7 +172,7 @@ const routes: Routes = [
         component:AddCategoryComponent
       },
       {
-        path: 'add-varient',
+        path: "add-varient/:id",
         component: AddVarientComponent
       },
       {
