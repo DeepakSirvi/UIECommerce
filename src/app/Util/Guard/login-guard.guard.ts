b2949dab4e385bcfff43cf7c0ee5033b1cdd7187
DeepakSirvi <deepaksirvi45@gmail.com>
2024-01-08 16:01:00 +0530

    Search

---
+++ src/app/Util/Guard/login-guard.guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { LoginService } from 'src/app/Service/AuthService/login.service';
+
+export const loginGuardGuard: CanActivateFn = (route, state) => {
+  const loginService = inject(LoginService);
+  const router = inject(Router);
+   if(loginService.isLoggedIn())
+   {
+    router.navigate(['home']);
+    return false;
+   }
+  return true;
+};
